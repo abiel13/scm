e4d2cce9be08b5bc04f0ee5736521900ce21428e
Abiel asimiea <dbestabi28@gmail.com>
2022-12-28 01:01:28 -1100

    charts

---
+++ src/components/lineChart.jsx
@@ -0,0 +1,87 @@
+
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+import { PieChart, Pie } from 'recharts';
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
+
+export default function Chart() {
+  return (
+    <>
+    <div className="hidden  md:flex relative h-1/4 items-center  left-1/4">
+ <LineChart  width={500} height={350} data={data}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#8884d8"
+        activeDot={{ r: 8 }}
+      />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
+  
+    <PieChart width={500} height={500}>
+          <Pie data={data} dataKey="pv" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+          <Pie data={data} dataKey="uv" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+        </PieChart>
+    </div> 
+    <p className="font-bold text-xl">Student Attendance Representation</p>
+    </>
+   );
+}
--- src/pages/SchoolDashBoard.jsx
+++ src/pages/SchoolDashBoard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Chart from '../components/lineChart';
+
 import SideBar from '../components/SideBar';
 
 function SchoolDashBoard() {
@@ -7,8 +9,9 @@ let params = useParams();
 
 
     return (
-  <div className='w-screen min-h-screen'>
+  <div className='w-screen min-h-screen flex overflow-x-hidden '>
 <SideBar name={params.name} />
+<Chart />
   </div>
     
   )
